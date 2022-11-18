aaa5ba653a14304a5b11b141981252fc33c433dc
Kevin Cauchois <kcauchois@ftel.fr>
2022-11-18 16:02:09 +0100

    Instanciation du converter dans la requete pour gérer l'async

--- index.mjs
+++ index.mjs
@@ -5,20 +5,23 @@ import axios from "axios";
 
 const serverPort = 3000;
 const app = express();
-const converter = convertSvgToPng.createConverter({
-  puppeteer: {
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
-  }
-});
+
 app.use(bodyParser.text({
   type: '*/*',
   limit: '10mb',
 }));
 
 app.get('/convert', async (req, res) => {
+  var converter;
   try {
+    converter = convertSvgToPng.createConverter({
+      puppeteer: {
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+      }
+    });
+
     const options = {};
-    options.background = req.query.background || '#fff';
+    if (req.query.background) options.background = req.query.background;
     options.baseUrl = req.query.baseUrl || 'file:///app';
     options.scale = req.query.scale || 1;
     if (req.query.baseFile) options.baseFile = req.query.baseFile;
@@ -38,25 +41,11 @@ app.get('/convert', async (req, res) => {
   } catch (e) {
     res.status(500).send(e.message);
     console.log('error', e);
-  }
-});
-
-app.post('/convert', async (req, res) => {
-  try {
-    const options = {};
-    options.background = req.query.background || '#fff';
-    options.baseUrl = req.query.baseUrl || 'file:///app';
-    options.scale = req.query.scale || 1;
-    if (req.query.baseFile) options.baseFile = req.query.baseFile;
-    if (req.query.height) options.height = req.query.height;
-    if (req.query.width) options.width = req.query.width;
-    console.log('convert svg', options);
-    const png = await converter.convert(req.body, options);
-    res.set('Content-Type', 'image/png');
-    res.send(png);
-  } catch (e) {
-    res.status(500).send(e.message);
-    console.log('error', e);
+  } finally {
+    if(converter) {
+      console.log('graceful shutdown puppeteer');
+      await converter.destroy();
+    }
   }
 });
 
@@ -68,8 +57,6 @@ server.on('connection', function (socket) {
 });
 
 const shutdown = async () => {
-  console.log('graceful shutdown puppeteer');
-  await converter.destroy();
   console.log('graceful shutdown express');
   server.close(function () {
     console.log('closed express');
